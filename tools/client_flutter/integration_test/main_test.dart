238032c55216f02e5a31e69392f3d033ae2bf140
David Wiebe <drwho.david.wiebe@gmail.com>
2025-09-25 19:01:55 -0700

    Initial update for fall 2025. Still should update some of the flutter pieces.

--- tools/client_flutter/integration_test/main_test.dart
+++ tools/client_flutter/integration_test/main_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:WebSocketDemo/main.dart';
+import 'package:websocketdemo/main.dart';
 
 
 // This performs thresholded retries while waiting for an element to appear.
